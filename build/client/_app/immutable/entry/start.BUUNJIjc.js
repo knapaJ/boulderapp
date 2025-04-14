038722f5ab0c686d906f48e3c77c4ae8026b6d57
jknapovsky <jknapovsky@gmail.com>
2025-04-14 11:46:49 +0200

    remove old build for 0.0.4

--- build/client/_app/immutable/entry/start.BUUNJIjc.js
+++
@@ -1,1 +0,0 @@
-import{l as o,d as r}from"../chunks/Co6FbBpI.js";export{o as load_css,r as start};
--- build/server/chunks/index5-_pYpgmRp.js
+++
@@ -1,8 +0,0 @@
-function sineIn(t) {
-  const v = Math.cos(t * Math.PI * 0.5);
-  if (Math.abs(v) < 1e-14) return 1;
-  else return 1 - v;
-}
-
-export { sineIn as s };
-//# sourceMappingURL=index5-_pYpgmRp.js.map
