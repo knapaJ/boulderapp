58bd74254344345274f1d0d58bdfca30e91d658e
jknapovsky <jknapovsky@gmail.com>
2025-04-15 15:25:33 +0200

    0.0.5 prepare, the leaderboards do not order by attempt count correctly SOMETIMES! Added disclaimer message as hotfix for now.

--- build/server/chunks/1-CzYEXXjS.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-De4pFWEk.js')).default;
-const imports = ["_app/immutable/nodes/1.CGT7bUWW.js","_app/immutable/chunks/ErH8zhpA.js","_app/immutable/chunks/BUBo3YVX.js","_app/immutable/chunks/C88kszIe.js","_app/immutable/chunks/uQ_J19qF.js","_app/immutable/chunks/BGMQuSJQ.js","_app/immutable/chunks/CfYLIdKV.js","_app/immutable/chunks/CtsFDf-N.js","_app/immutable/chunks/CGwxGfu1.js","_app/immutable/chunks/0XRK0UKb.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-CzYEXXjS.js.map
--- build/server/chunks/4-B7W28ESp.js
+++
@@ -1,9 +0,0 @@
-const index = 4;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-C66JQgk7.js')).default;
-const imports = ["_app/immutable/nodes/4.BP6QHnFj.js","_app/immutable/chunks/ErH8zhpA.js","_app/immutable/chunks/BUBo3YVX.js","_app/immutable/chunks/DPT-C-Kk.js","_app/immutable/chunks/CGwxGfu1.js","_app/immutable/chunks/B1TSEx5U.js","_app/immutable/chunks/C88kszIe.js","_app/immutable/chunks/BY-NkPsp.js","_app/immutable/chunks/BGMQuSJQ.js","_app/immutable/chunks/BY63YvFM.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=4-B7W28ESp.js.map
